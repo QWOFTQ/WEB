f87515ab8b4c33437108dddc2a6bdffa43e71318
QWOFTQ <rkdcofls3890@naver.com>
2022-10-12 11:14:11 +0900

    1st

--- pages/profile/bclee.js
+++ pages/profile/bclee.js
@@ -2,7 +2,6 @@ export default function BcleePage() {
   return (
     <div>
       BCLEE의 페이지입니다!
-      <p>접속하는 방법 ➡ /pages/profile/bclee.js</p>
     </div>
   )
 }
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,8 +1,12 @@
+import styles from '../../styles/Home.module.css'
+
 export default function ProfilePage() {
   return (
-    <div>
+    <div className={styles.about}>
       <h1>Profile Page</h1>
-      <h2> /pages/profile/index.js</h2>
+      <h2>BCLEE 페이지로 접속하는 방법 ➡ /profile/bclee</h2>
+      <h2>USER 페이지로 접속하는 방법 ➡ /profile/username </h2>
+
     </div>
   )
 }
