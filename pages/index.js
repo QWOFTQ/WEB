d7b6f057dc98c922231ced0d5026ca5d429e3298
QWOFTQ <rkdcofls3890@naver.com>
2022-10-12 09:42:40 +0900

    1st

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-      <h1 className={styles.title}>Hello World!</h1>
+      <h1 className={styles.title}>Home Page</h1>
       </main>
     </div>
   )
