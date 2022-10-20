708bce7966bc8b34924f7a94d9abba0458b08d14
QWOFTQ <rkdcofls3890@naver.com>
2022-10-20 11:39:50 +0900

    1st

--- pages/contact.js
+++ pages/contact.js
@@ -8,11 +8,7 @@ export default function Contact() {
   return (
     <div className={styles.me}>
       <h1>자기 소개</h1>
-      <Image
-      src={chaerin}
-      alt=''
-      width={200}
-      height={250}/>
+      
       <p>안녕하세요 반갑습니다!</p>
       <p>이름 : 강채린</p>
       <p>나이 : 21세</p>   
